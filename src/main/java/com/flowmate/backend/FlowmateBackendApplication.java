ca4437d955b6c3b496e2843e028e04f806b33797
Y-G-Lee <lyg0095@naver.com>
2026-07-01 17:21:26 +0900

    init project

---
+++ src/main/java/com/flowmate/backend/FlowmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flowmate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowmateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowmate/backend/FlowmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowmate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
